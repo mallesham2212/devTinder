3789f2caf4717d8b94ff0f98f9ec76355d744692
mallesham2212 <malleshamprojects22@gmail.com>
2024-11-18 12:49:21 +0530

    middle_wares_in_node

--- src/app.js
+++ src/app.js
@@ -1,20 +1,54 @@
 const express = require('express')
 const app = express()
 
+const {adminAuth,userAuth} =require("./middleware/adminAuth")
 
 
-app.get('/', function (req, res) {
-  res.send('Hello World  ')
+app.use("/admin",adminAuth);
+
+app.get("/admin/send",(req,res)=>{
+	res.send("send all data");
 })
-app.get('/home', function (req, res) {
-    res.send('Hello World  from home ')
-  })
 
-  app.get('/contact', function (req, res) {
-    res.send('Hello World  from contact ')
-  })
+app.use("/user",userAuth,(req,res)=>{
+	res.send("user data sending");
+})
 
-app.listen(4000,()=>{
-    console.log("server connecting...");
-    
+app.get("/admin/delete",(req,res)=>{
+	res.send("delete the user");
+	
+})
+
+// app.get("/", (req, res, next) => {
+// 	next();
+// 	// res.send('Hello World 1  ')
+// })
+
+// app.get('/', (req, res, next) => {
+// 	next();
+// },
+// 	(req, res, next) => {
+// 		next();
+// 	},
+// 	(req, res, next) => {
+// 		next();
+// 	},
+// 	(req, res, next) => {
+// 		next();
+// 	},
+// 	(req, res) => {
+// 		res.send('Hello World eui6ty');
+// 	}
+// )
+
+// app.get('/home', function (req, res) {
+// 	res.send('Hello World  from home ')
+// })
+
+// app.use('/contact', function (req, res) {
+// 	res.send('Hello World  from contact ')
+// })
+
+app.listen(4000, () => {
+	console.log("server connecting...");
 })
